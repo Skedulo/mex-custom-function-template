dffcfdb232f6ab469ef7e52a79bb641ef26a668c
Giang Nguyen <68680776+GiangNguyenn@users.noreply.github.com>
2023-05-30 13:47:46 +0700

    Update post_gen_project.sh

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -6,7 +6,7 @@ cd "{{ cookiecutter.project_name }}"
 # Initialize and update the submodule
 git init
 git checkout -b main
+
 git submodule add git@github.com:Skedulo/mex-custom-function-lib.git mex-custom-function-lib
 git submodule init
-
-cd mex-custom-function-lib; git checkout main; git pull --recurse-submodules; cd ..
+git submodule update
