4d9a932fdc025b212663068b68d23658e8f5a4eb
ncd <dinh.nguyen@skedulo.com>
2024-07-30 17:22:43 +0700

    ENG-49654 Fix issue

--- {{ cookiecutter.project_slug }}/custom-function/src/tools/generate-info.ts
+++ {{ cookiecutter.project_slug }}/custom-function/src/tools/generate-info.ts
@@ -1,7 +1,12 @@
 import fs from 'fs'
+import { Function } from '@skedulo/mex-service-libs/functions'
 import { version } from '@skedulo/mex-service-libs/package.json'
 import { CustomFunctionInfo } from '@skedulo/mex-service-libs/types'
 import { params } from '../params'
+
+// Workaround for tsyringe polyfill issue
+// @ts-ignore
+const skeduloFunction = new Function(params)
 
 const info: CustomFunctionInfo = {
     libVersion: version,
